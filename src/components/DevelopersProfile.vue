d0ee95a5d8b9e26ac1560a5ed2b8df7860a7be1b
jalandonije_sd2023@CEACCS.NET <jalandonije_sd2023@CEACCS.NET>
2019-10-29 17:36:13 +0800

    comit

--- src/components/DevelopersProfile.vue
+++ src/components/DevelopersProfile.vue
@@ -1,8 +1,5 @@
-<template>
-  <div class="about">
-    <h1>Toothbook Developers</h1>
-    <div>
-  <h5>The <code>DEVELOPERS</code>:</h5>
+  <template>
+  <div>
   <img  alt="Vue logo" src="@/assets/biyang.jpg">
   <h4> Ivy Devilleres</h4>
   <img alt="Vue logo" src="@/assets/Jess.jpg">
@@ -10,9 +7,13 @@
   <img alt="Vue logo" src="@/assets/Gen3.png">
   <h4>Geneva Rivas</h4>
   <img alt="Vue logo" src="@/assets/Gen3.png">
-  <h4>Mary Grace Tiburillo</h4>
   </div>
-  </div>
-  </div>
-    </div>
-</template>
+  </template>
+  <script>
+  	export default {
+  name: 'devProfile',
+  components: {
+    HelloWorld
+  }
+}
+  </script>
